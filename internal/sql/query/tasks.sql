a38c7f4055281a73ff328b5f05221bb1c0ac9d3a
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-04-17 09:35:45 +0000

    Update to sqlc generate

---
+++ internal/sql/query/tasks.sql
@@ -0,0 +1,21 @@
+-- name: ScheduledTasks :many
+WITH RECURSIVE rec_project_name(id, name, level) AS (
+  SELECT id, name, 1 AS level FROM projects WHERE parent_id IS NULL
+  UNION ALL
+  SELECT
+    projects.id,
+    rec_project_name.name||' > '||projects.name,
+    rec_project_name.level + 1
+  FROM projects
+  JOIN rec_project_name ON projects.parent_id = rec_project_name.id
+)
+SELECT tasks.id, tasks.name, project_id, planned_for, start_at, due_at, rec_project_name.name AS project_name, rec_project_name.level AS project_level
+FROM tasks
+JOIN rec_project_name ON tasks.project_id = rec_project_name.id
+WHERE planned_for <= DATE() AND completed_at IS NULL ORDER BY planned_for, start_at NULLS LAST, due_at NULLS LAST;
+
+-- name: StartTask :exec
+UPDATE tasks SET start_at = IIF(start_at IS NULL, DATE(), start_at) WHERE id = ?;
+
+-- name: GetTask :one
+SELECT * FROM tasks WHERE id = ?;
---
+++ internal/sql/query/time_entry.sql
@@ -0,0 +1,23 @@
+-- name: StartTimeTracking :one
+INSERT INTO time_entries (task_id, start_at) VALUES (?, ?)
+RETURNING *;
+
+-- name: StopTimeTracking :exec
+UPDATE time_entries SET end_at = ? WHERE end_at IS NULL;
+
+-- name: GetRunningTimer :one
+SELECT start_at, task_id FROM time_entries WHERE end_at IS NULL;
+
+-- name: EnterTimeEntry :one
+INSERT INTO time_entries (task_id, start_at, end_at) VALUES (?, ?, ?)
+RETURNING *;
+
+-- name: UpdateTimeEntry :one
+UPDATE time_entries SET task_id = ?, start_at = ?, end_at = ? WHERE id = ?
+RETURNING *;
+
+-- name: ListTimeEntries :many
+SELECT start_at, end_at, task_id FROM time_entries WHERE ? BETWEEN start_at AND due_at;
+
+-- name: TimeByTask :many
+SELECT start_at, end_at FROM time_entries WHERE task_id = ?;
