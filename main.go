ca3c6563b7f312a15b304cb190af99a349bbae0b
Rasmus Frederiksen <rasmus@ufafarion.com>
2025-07-02 14:34:08 +0000

    Quit on q by default

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
+		case "q":
 			return m, tea.Quit
 		case "ctrl+t":
 			if m.entry.TaskID > 0 {
